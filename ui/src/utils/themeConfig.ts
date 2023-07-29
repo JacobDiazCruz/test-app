6d4a97f4b29477c9d2b1718dc7a87f43fbc5447a
JacobDiazCruz <carljacobdiazcruz@gmail.com>
2023-07-29 14:36:05 +0800

    Abstract mui theme config from app

---
+++ ui/src/utils/themeConfig.ts
@@ -0,0 +1,85 @@
+import { createTheme, Theme } from "@mui/material";
+
+const palette = {
+  primary: {
+    main: "#2d63ff"
+  },
+  success: {
+    main: "#007f25",
+  },
+  text: {
+    primary: "#000",
+    secondary: "#a3a4a5",
+  },
+}
+
+export const theme: Theme = createTheme({
+  palette,
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          boxShadow: '0 2px 8px #ebeef2',
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+          boxShadow: 'none',
+          borderRadius: '8px',
+          height: '45px',
+          '&:hover': {
+            opacity: 0.9
+          }
+        }
+      }
+    },
+    MuiBackdrop: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "rgba(0, 0, 0, 0)"
+        },
+      },
+    },
+    MuiTableCell: {
+      styleOverrides: {
+        root: {
+          paddingLeft: 30
+        }
+      }
+    },
+    MuiTableHead: {
+      styleOverrides: {
+        root: {
+          "& .MuiTableCell-root": {
+            fontWeight: "bold"
+          },
+        },
+      },
+    },
+    MuiSvgIcon: {
+      styleOverrides: {
+        root: {
+          "& .Mui-completed": {
+            color: '#007f25'
+          }
+        }
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          height: '45px',
+          backgroundColor: 'white',
+          '& fieldset': {
+            borderColor: '#e0e0e0',
+            borderWidth: '1px',
+            borderRadius: '4px'
+          },
+        }
+      }
+    }
+  }
+});
