041175166d0f8edad1176d78e33d03069fc2def2
JacobDiazCruz <carljacobdiazcruz@gmail.com>
2023-07-29 11:30:40 +0800

    Added: Create new client form UI

--- ui/src/pages/Clients/CreateNewClientModal.tsx
+++ ui/src/pages/Clients/CreateNewClientModal.tsx
@@ -1,8 +1,8 @@
 import { Close } from "@mui/icons-material";
-import { IconButton, Modal, Step, StepButton, Stepper, Typography } from "@mui/material";
+import { Button, IconButton, Modal, Step, StepButton, Stepper, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useState } from "react";
-
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 interface Props {
   open: boolean;
   handleClose: () => void;
@@ -31,8 +31,45 @@ export default function CreateNewClientModal({
     [k: number]: boolean;
   }>({});
 
+  const [form, setForm] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    contact: ''
+  });
+
   const handleStep = (step: number) => () => {
     setActiveStep(step);
+  };
+
+  const totalSteps = () => {
+    return steps.length;
+  };
+
+  const completedSteps = () => {
+    return Object.keys(completed).length;
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps();
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleNext = () => {
+    const newActiveStep =
+      isLastStep() && !allStepsCompleted()
+        ? // It's the last step, but not all steps have been completed,
+          // find the first step that has been completed
+          steps.findIndex((step, i) => !(i in completed))
+        : activeStep + 1;
+    setActiveStep(newActiveStep);
   };
 
   return (
@@ -61,7 +98,101 @@ export default function CreateNewClientModal({
             </Step>
           ))}
         </Stepper>
+        {activeStep === 0 ? (
+          <Box py={3}>
+            <Box>
+              <Typography>
+                First name
+              </Typography>
+              <TextField 
+                sx={{ width: '100%' }}
+                value={form.firstName}
+                onChange={(e) => {
+                  setForm((prev) => ({
+                    ...prev,
+                    firstName: e.target.value
+                  }))
+                }}
+              />
+            </Box>
+            <Box mt={2}>
+              <Typography>
+                Last name
+              </Typography>
+              <TextField 
+                sx={{ width: '100%' }}
+                value={form.lastName}
+                onChange={(e) => {
+                  setForm((prev) => ({
+                    ...prev,
+                    lastName: e.target.value
+                  }))
+                }}
+              />
+            </Box>
+          </Box>
+        ) : (
+          <Box py={3}>
+            <Box>
+              <Typography>
+                Email
+              </Typography>
+              <TextField 
+                sx={{ width: '100%' }}
+                value={form.email}
+                onChange={(e) => {
+                  setForm((prev) => ({
+                    ...prev,
+                    email: e.target.value
+                  }))
+                }}
+              />
+            </Box>
+            <Box mt={2}>
+              <Typography>
+                Contact
+              </Typography>
+              <TextField 
+                sx={{ width: '100%' }}
+                value={form.contact}
+                onChange={(e) => {
+                  setForm((prev) => ({
+                    ...prev,
+                    contact: e.target.value
+                  }))
+                }}
+              />
+            </Box>
+          </Box>
+        )}
+        <Box sx={{ display: 'flex' }}>
+          {activeStep === 1 && (
+            <Button
+              onClick={handleBack}
+              startIcon={<ArrowBackIcon />}
+            >
+              Back
+            </Button>
+          )}
+
+          {activeStep === 0 ? (
+            <Button
+              sx={{ ml: 'auto' }}
+              variant="contained"
+              onClick={handleNext}
+            >
+              Continue
+            </Button>
+          ) : (
+            <Button
+              sx={{ ml: 'auto' }}
+              variant="contained"
+            >
+              Create client
+            </Button>
+          )}
+        </Box>
       </Box>
     </Modal>
   );
-}
+};
